2f0d24aed760ef6a0f1cf7e30130f289a0f67fa3
azmigproject <azmigproject@outlook.com>
2017-06-29 13:55:01 +0530

    Edited

--- cn-setup.sh
+++ cn-setup.sh
@@ -18,7 +18,7 @@ log()
 	echo "$1"
 }
 
-usage() { echo "Usage: $0 [-m <masterName>] [-s <pbspro>] [-q <queuename>] [-S <beegfs, nfsonmaster>] [-n <ganglia>] [-c <postInstallCommand>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-m <masterName>]" 1>&2; exit 1; }
 
 while getopts :m:S:s:q:n:c: optname; do
   log "Option $optname set with value ${OPTARG}"
@@ -54,18 +54,6 @@ is_centos()
 	return $?
 }
 
-is_suse()
-{
-	python -mplatform | grep -qi Suse
-	return $?
-}
-
-is_ubuntu()
-{
-	python -mplatform | grep -qi Ubuntu
-	return $?
-}
-
 mount_nfs()
 {
 	log "install NFS"
@@ -86,31 +74,6 @@ mount_nfs()
 	
 	echo "${MASTER_NAME}:${NFS_ON_MASTER} ${NFS_MOUNT} nfs defaults,nofail  0 0" >> /etc/fstab
 }
-
-install_beegfs_client()
-{
-	bash install_beegfs.sh ${MASTER_NAME} "client"
-}
-
-install_ganglia()
-{
-	bash install_ganglia.sh ${MASTER_NAME} "Cluster" 8649
-}
-
-install_pbspro()
-{
-	bash install_pbspro.sh ${MASTER_NAME} ${QNAME}
-}
-
-install_blobxfer()
-{
-	if is_centos; then
-		yum install -y gcc openssl-devel libffi-devel python-devel
-		curl https://bootstrap.pypa.io/get-pip.py | python
-		pip install --upgrade blobxfer
-	fi
-}
-
 setup_user()
 {
 	if is_centos; then
@@ -140,19 +103,6 @@ setup_user()
 
     chown $HPC_USER:$HPC_GROUP $SHARE_SCRATCH	
 }
-
-setup_intel_mpi()
-{
-	if is_suse; then
-		if [ -d "/opt/intelMPI" ]; then
-			rpm -v -i --nodeps /opt/intelMPI/intel_mpi_packages/*.rpm
-			impi_version=`ls /opt/intel/impi`
-			ln -s /opt/intel/impi/${impi_version}/intel64/bin/ /opt/intel/impi/${impi_version}/bin
-			ln -s /opt/intel/impi/${impi_version}/lib64/ /opt/intel/impi/${impi_version}/lib
-		fi		
-	fi
-}
-
 mkdir -p /var/local
 SETUP_MARKER=/var/local/cn-setup.marker
 if [ -e "$SETUP_MARKER" ]; then
@@ -165,44 +115,8 @@ if is_centos; then
 	sed -i 's/enforcing/disabled/g' /etc/selinux/config
 	setenforce permissive
 fi
-
-if is_ubuntu; then
-	# there is an issue here because apt may be already running the first time the machine is booted
-	while true;
-	do
-		if [[ $(ps -A | grep -c apt)  -ne 1 ]]; then
-			echo "apt is running, wait 1m"
-		else
-			break
-		fi
-		sleep 1m
-	done
-fi
-
 setup_user
-if [ "$MONITORING" == "ganglia" ]; then
-	install_ganglia
-fi
-
-if [ "$SCHEDULER" == "pbspro" ]; then
-	install_pbspro
-fi
-
-if [ "$SHARED_STORAGE" == "beegfs" ]; then
-	install_beegfs_client
-elif [ "$SHARED_STORAGE" == "nfsonmaster" ]; then
-	mount_nfs
-fi
-
-setup_intel_mpi
-#install_blobxfer
-
-if [ -n "$POST_INSTALL_COMMAND" ]; then
-	echo "running $POST_INSTALL_COMMAND"
-	eval $POST_INSTALL_COMMAND
-fi
+mount_nfs
 # Create marker file so we know we're configured
 #touch $SETUP_MARKER
-
-#shutdown -r +1 &
 exit 0
