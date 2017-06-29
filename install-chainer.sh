6b7169cadca572f10920e13c67a72b4e16cd6a78
azmigproject <azmigproject@outlook.com>
2017-06-29 15:02:10 +0530

    Updated

--- install-chainer.sh
+++ install-chainer.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Shares
+SHARE_HOME=/share/home
+SHARE_SCRATCH=/share/scratch
+NFS_ON_MASTER=/data
+NFS_MOUNT=/data
 
+# User
+HPC_USER=hpcuser
+HPC_UID=7007
+HPC_GROUP=hpc
+HPC_GID=7007
 
 #############################################################################
 log()
@@ -8,7 +18,7 @@ log()
 	echo "$1"
 }
 log "$0,$1,$2,$3"
-usage() { echo "Usage: $0 [-m ] " 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-m ] [-s <masterName>] " 1>&2; exit 1; }
 
 while getopts :m: optname; do
   log "Option $optname set with value ${OPTARG}"
@@ -16,6 +26,9 @@ while getopts :m: optname; do
   case $optname in
     m)  # master name
 		export CHAINER_MN=${OPTARG}
+		;;
+    s)  # master name
+		export MASTER_NAME=${OPTARG}
 		;;
 	*)
 		usage
@@ -26,6 +39,47 @@ is_centos()
 {
 	python -mplatform | grep -qi CentOS
 	return $?
+}
+mount_nfs()
+{
+	log "install NFS"
+
+	if is_centos; then
+		yum -y install nfs-utils nfs-utils-lib	
+	fi
+	
+	mkdir -p ${NFS_MOUNT}
+
+	log "mounting NFS on " ${MASTER_NAME}
+	showmount -e ${MASTER_NAME}
+	mount -t nfs ${MASTER_NAME}:${NFS_ON_MASTER} ${NFS_MOUNT}
+	
+	echo "${MASTER_NAME}:${NFS_ON_MASTER} ${NFS_MOUNT} nfs defaults,nofail  0 0" >> /etc/fstab
+}
+setup_user()
+{
+	if is_centos; then
+		yum -y install nfs-utils nfs-utils-lib	
+	fi
+
+    mkdir -p $SHARE_HOME
+    mkdir -p $SHARE_SCRATCH
+
+	echo "$MASTER_NAME:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+	mount -a
+	mount
+   
+    groupadd -g $HPC_GID $HPC_GROUP
+
+    # Don't require password for HPC user sudo
+    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    
+    # Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+
+	useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+
+    chown $HPC_USER:$HPC_GROUP $SHARE_SCRATCH	
 }
 
 check_docker()
@@ -135,6 +189,14 @@ if [ -e "$SETUP_MARKER" ]; then
     echo "We're already configured, exiting..."
     exit 0
 fi
+
+if is_centos; then
+	# disable selinux
+	sed -i 's/enforcing/disabled/g' /etc/selinux/config
+	setenforce permissive
+fi
+setup_user
+mount_nfs
 check_docker
 
 if [ "$CHAINER_MN" == "1" ]; then
