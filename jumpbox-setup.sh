af640e83d54fe2cf4156ad969b0fe5adcd0df384
azmigproject <azmigproject@outlook.com>
2017-06-29 10:27:57 +0530

    Updated

--- jumpbox-setup.sh
+++ jumpbox-setup.sh
@@ -78,29 +78,6 @@ setup_user()
 	
 	chown $HPC_USER:$HPC_GROUP $SHARE_SCRATCH
 }
-setup_user2()
-{
-    mkdir -p $SHARE_HOME
-    mkdir -p $SHARE_SCRATCH
-
-	echo "$MGMT_HOSTNAME:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/exports
-	mount -a
-	mount
-   
-    groupadd -g $HPC_GID $HPC_GROUP
-
-    # Don't require password for HPC user sudo
-    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    
-    # Disable tty requirement for sudo
-    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
-
-	useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
-
-    chown $HPC_USER:$HPC_GROUP $SHARE_SCRATCH
-	chown $HPC_USER:$HPC_GROUP $SHARE_APPS	
-
-}
 
 mount_nfs()
 {
