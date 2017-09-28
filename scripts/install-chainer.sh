2861ef6faad36c7242f5508a2d845a721a7cd720
azmigproject <azmigproject@outlook.com>
2017-09-28 18:17:59 +0530

    Updated

--- scripts/install-chainer.sh
+++ scripts/install-chainer.sh
@@ -69,8 +69,8 @@ base_pkgs_ubuntu()
       
        # enable rdma
        cd /etc/
-       sudo sed -i  's|${OLD_CONTENT}|${NEW_CONTENT}|g;' waagent.conf
-       sudo sed -i  's|${OLD_CONTENTUP}|${OLD_CONTENTUP}|g;' waagent.conf
+       sudo sed -i  's|# OS.EnableRDMA=y|OS.EnableRDMA=y|g;' waagent.conf
+       sudo sed -i  's|# OS.UpdateRdmaDriver=y|OS.UpdateRdmaDriver=y|g;' waagent.conf
        
        # WALinux Agent Installation
 	git clone https://github.com/Azure/WALinuxAgent.git
