307c3392912decc0f0c635645db57ba7d4acca8a
Shunta Saito <shunta.saito@gmail.com>
2017-12-19 00:35:04 +0900

    Remove files

--- scripts/prerequisite.sh
+++
@@ -1,5 +0,0 @@
-yum -y install telnet
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
-yum -y install jq
-yum install -y gcc libffi-devel python-devel openssl-devel
-curl -L https://aka.ms/InstallAzureCli | bash 
