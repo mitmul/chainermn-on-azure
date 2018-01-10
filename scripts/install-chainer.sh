37346fe6e2197100a1b992b157071ce1094e0abe
Shunta Saito <shunta.saito@gmail.com>
2018-01-10 22:41:45 +0900

    Fix installation of CUDA

--- scripts/install-chainer.sh
+++ scripts/install-chainer.sh
@@ -101,7 +101,7 @@ setup_cuda()
 	sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
 	rm -f /tmp/${CUDA_REPO_PKG}
 	sudo apt-get update
-	sudo apt-get install cuda-drivers
+	sudo apt-get install -y cuda-drivers
 
 	if [ $CUDA_VERSION = 8.0 ]; then
 		sudo apt-get install -y cuda-8-0
