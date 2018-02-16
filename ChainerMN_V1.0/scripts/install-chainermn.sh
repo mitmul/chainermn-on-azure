ac4eb882ea5b37c1e18ffaf7ca025a2c9b076262
azmigproject <azmigproject@outlook.com>
2018-02-16 13:25:23 +0530

    Updated for  intel MPI 5.1.3.223

--- ChainerMN_V1.0/scripts/install-chainermn.sh
+++ ChainerMN_V1.0/scripts/install-chainermn.sh
@@ -85,10 +85,14 @@ install_intel_mpi
 		if [ ! -d /opt/l_mpi_2017.3.196 ]; then
 			cd /opt
 			sudo mv intel intel_old
-			sudo curl -L -O http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/11595/l_mpi_2017.3.196.tgz
-			sudo tar zxvf l_mpi_2017.3.196.tgz
-			sudo rm -rf l_mpi_2017.3.196.tgz
-			cd l_mpi_2017.3.196
+			#sudo curl -L -O http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/11595/l_mpi_2017.3.196.tgz
+			#sudo tar zxvf l_mpi_2017.3.196.tgz
+			#sudo rm -rf l_mpi_2017.3.196.tgz
+			#cd l_mpi_2017.3.196
+			sudo curl -L -O https://pfnresources.blob.core.windows.net/chainermn-v1-packages/l_mpi-rt_p_5.1.3.223.tgz.gz
+			sudo tar zxvf l_mpi-rt_p_5.1.3.223.tgz
+			sudo rm -rf l_mpi-rt_p_5.1.3.223.tgz
+			cd l_mpi-rt_p_5.1.3.223.tgz
 			sudo sed -i -e "s/decline/accept/g" silent.cfg
 			sudo ./install.sh --silent silent.cfg
 		fi
