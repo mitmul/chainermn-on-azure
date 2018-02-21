573963bdded38d4fc6c5f4ca69bed59d263f83b9
azmigproject <azmigproject@outlook.com>
2018-02-21 14:39:56 +0530

    Updated

--- ChainerMN_V1.0/scripts/install-chainermn.sh
+++ ChainerMN_V1.0/scripts/install-chainermn.sh
@@ -100,6 +100,8 @@ install_intel_mpi
 			sudo rm -rf l_mpi_p_5.1.3.223.tgz
 			cd l_mpi_p_5.1.3.223
 			sudo sed -i -e "s/decline/accept/g" silent.cfg
+			sudo sed -i -e "s/#ACTIVATION_SERIAL_NUMBER=snpat/ACTIVATION_SERIAL_NUMBER=VTDT-4246MB9S/g" silent.cfg
+			sudo sed -i -e "s/ACTIVATION_TYPE=exist_lic/ACTIVATION_TYPE=serial_number/g" silent.cfg
 			sudo ./install.sh --silent silent.cfg
 		fi
 
