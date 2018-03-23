6acccbcbd84154c29aed7141150c56e467c082e6
azmigproject <azmigproject@outlook.com>
2018-03-23 18:57:23 +0530

    Updated for path variables

--- ChainerMN_V1.0/scripts/install-chainermn.sh
+++ ChainerMN_V1.0/scripts/install-chainermn.sh
@@ -271,11 +271,11 @@ setup_chainermn_gpu_infiniband()
 		install_chainermn
 		alias python=python3
 		
-		echo 'LD_LIBRARY_PATH=/usr/local/lib:/usr/local/cuda/lib64:${LD_LIBRARY_PATH}' >> ~hpcuser/.bashrc
-		echo 'LIBRARY_PATH=/usr/local/lib:/usr/local/cuda/lib64:${LIBRARY_PATH}' >> ~hpcuser/.bashrc
-		echo 'CPATH=/usr/loca/include]/usr/loca/cuda/include:${CPATH}' >> ~hpcuser/.bashrc
+		echo 'export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/cuda/lib64:${LD_LIBRARY_PATH}' >> ~hpcuser/.bashrc
+		echo 'export LIBRARY_PATH=/usr/local/lib:/usr/local/cuda/lib64:${LIBRARY_PATH}' >> ~hpcuser/.bashrc
+		echo 'export CPATH=/usr/loca/include]/usr/loca/cuda/include:${CPATH}' >> ~hpcuser/.bashrc
 		echo './opt/intel/compilers_and_libraries_2016.3.223/linux/mpi/intel64/bin/mpivars.sh' >> ~hpcuser/.bashrc
-		echo 'PATH=/opt/anaconda3/bin:${PATH}' >> ~hpcuser/.bashrc
+		echo 'export PATH=/opt/anaconda3/bin:${PATH}' >> ~hpcuser/.bashrc
 
 
 }
